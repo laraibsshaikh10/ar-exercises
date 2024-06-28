726661c10eb016066e655ef6f7b4172a149f8365
laraibsshaikh10 <laraibsshaikh10@gmail.com>
2024-06-28 18:56:17 -0400

    Complete exercise 2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,15 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+# Load the first store
+@store1 = Store.find(1)
+
+# Load the second store
+@store2 = Store.find(2)
+
+# Update the first store
+@store1.update(name: "Updated Burnaby")
+
+# Alternatively, you could do it in two steps:
+# @store1.name = "Updated Burnaby"
+# @store1.save

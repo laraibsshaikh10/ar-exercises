58914bac5e9247421896b5433297906233c78201
laraibsshaikh10 <laraibsshaikh10@gmail.com>
2024-06-28 18:58:58 -0400

    Delete the third store

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -6,3 +6,11 @@ puts "Exercise 3"
 puts "----------"
 
 # Your code goes here ...
+# Load the third store
+@store3 = Store.find(3)
+
+# Delete the third store
+@store3.destroy
+
+# Verify the store has been deleted by outputting the count
+puts Store.count
